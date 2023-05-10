97ac751c6bdfa30d2c44853d59a67d09b3aa5b37
Khairul Rizma <40055460+khairulrizma@users.noreply.github.com>
2023-05-10 22:58:55 +0800

    Add files via upload

---
+++ MagicEightBall.js
@@ -0,0 +1,42 @@
+// The user will be able to input a question, then our program will output a random fortune.
+
+//create switch statement
+var userName = 'Jane'
+
+userName === 'Jane' ? console.log('Hello, Jane!') : console.log('Hello!')
+
+var userQuestion = `Am ${userName} good at football?`
+
+console.log(`Question : ${userQuestion}`)
+
+const randomNumber = Math.floor(Math.random() * 8)
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain'
+    break
+  case 1:
+    eightBall = 'It is decidedly so'
+    break
+  case 2:
+    eightBall = 'Reply hazy try again'
+    break
+  case 3:
+    eightBall = 'Cannot predict now'
+    break
+  case 4:
+    eightBall = 'Do not count on it'
+    break
+  case 5:
+    eightBall = 'My sources say no'
+    break
+  case 6:
+    eightBall = 'Outlook not so good'
+    break
+  case 7:
+    eightBall = 'Signs point to yes'
+    break
+}
+
+console.log(`The eight ball answered:
+ ${eightBall}`)
